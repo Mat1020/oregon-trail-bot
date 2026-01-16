9a2dd977776c503f8a06cfa575c3c50996869d35
Mat1020 <matias.tajan@gmail.com>
2026-01-15 22:41:25 -0500

    Created a simple system to run the bots

--- Main/main.py
+++ Main/main.py
@@ -1,1 +1,32 @@
 import time, Bots
+
+bots = {
+    "start for speed running bot": 1
+}
+
+def loop_available_bots():
+    for bot_name, bot_id in bots.items():
+        print(f"{bot_name.title()}: {bot_id}")
+
+def boot_bot():
+    while True:
+        id = input("Enter the ID of the bot you want to boot up: ")
+        if id.isdigit():
+            id = int(id)
+            if 1 <= id <= len(bots):
+                break
+            else:
+                print("Please enter a valid bot ID next time.")
+                time.sleep(0.5)
+        else:
+            print("Please enter a number for a bot ID next time.")
+            time.sleep(0.5)
+
+        if id == 1:
+            Bots.start_for_speed_running_bot.main()
+
+def main():
+    print("This is where you boot up a bot. These are the available bots: ")
+    time.sleep(1)
+    loop_available_bots()
+    boot_bot()
